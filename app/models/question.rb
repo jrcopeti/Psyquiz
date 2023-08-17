f1eb25bb8d62dcd7260e0f63ddea0f048fcc9476
Jose Copeti <jrcopeti@gmail.com>
2023-08-17 16:38:35 +0200

    refactor forms to simple form

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   validates :correct_answer, inclusion: { in: 1..4 }
+  validates :content, :answer1, :answer2, :answer3, :answer4, presence: true
   has_one_attached :image
 end
